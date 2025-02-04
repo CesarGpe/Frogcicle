9f8b1ecc75577cb7627778d64f42d1157496f784
CesarGpe <57163912+CesarGpe@users.noreply.github.com>
2025-02-04 02:56:49 -0700

    working on pc

--- conf.lua
+++ conf.lua
@@ -2,15 +2,18 @@ WIDTH = 1280
 HEIGHT = 720
 
 function love.conf(t)
-	t.identity         = "frogcicle"
-	t.appendidentity   = false
-	t.externalstorage  = true
-	t.gammacorrect     = false
+	t.identity          = "frogcicle"
+	t.appendidentity    = false
+	t.externalstorage   = true
+	t.gammacorrect      = false
 
-	t.window.title     = "Frogcicle!"
-	t.window.icon      = "assets/sprites/icon.png"
-	t.window.width     = WIDTH
-	t.window.height    = HEIGHT
-	t.window.minwidth  = 0
-	t.window.minheight = 0
+	t.window.title      = "Frogcicle!"
+	t.window.icon       = "assets/sprites/icon.png"
+	t.window.width      = WIDTH
+	t.window.height     = HEIGHT
+	t.window.fullscreen = false
+	t.window.borderless = false
+	t.window.resizable  = true
+	t.window.minwidth   = 100
+	t.window.minheight  = 100
 end
